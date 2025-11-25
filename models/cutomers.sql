2ddb81a81038d91a7622ef9e7fa297d42d2e5fd4
odarbandi@wisc.edu <odarbandi@wisc.edu>
2025-11-25 21:05:24 +0000

    Defined sources

---
+++ models/cutomers.sql
@@ -0,0 +1,18 @@
+SELECT
+
+    customer_id,
+    first_name,
+    last_name,
+    city,
+    country,
+    registration_date,
+    DATEDIFF(year, registration_date, CURRENT_DATE()) AS years_as_customer,
+    birth_date,
+    DATEDIFF(year, birth_date, CURRENT_DATE()) AS age_years,
+    email,
+    phone_number
+
+FROM {{ ref('stg_salesforce__customers') }}
+FROM {{source}('salesforce','trucks'}
+FROM {{source}('salesforce','customers'}
+FROM {{source}('square','orders'}
